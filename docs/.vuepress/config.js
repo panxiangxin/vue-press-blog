ccef9287ab78d6a76b43a7abd3da1a49a5b3b105
panxiangxin <www.721297578@qq.com>
2022-04-19 21:48:21 +0800

    初次提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: 'Code Today',
+    description: 'Just Coding!',
+    themeConfig: {
+        sidebar: [
+            '/',
+            '/leetcode/821',
+        ]
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,9 @@
+---
+home: true
+heroImage: /logo.png
+heroText: PXX 的刷题笔记📒
+tagline: Code EveryDay!
+actionText: 快速上手 →
+actionLink: /leetcode/821.md
+footer: www.www.www
+---
